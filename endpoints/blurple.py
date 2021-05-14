714f9314b9882cee8503dbaaa035bda721621cb1
Snazzah <suggesttosnazzy@gmail.com>
2021-05-14 17:08:00 -0500

    Add new color setting for blurple filter

--- endpoints/blurple.py
+++ endpoints/blurple.py
@@ -6,9 +6,15 @@ from utils.endpoint import SimpleFilter, setup
 @setup
 class Blurple(SimpleFilter):
     def use_filter(self, img, kwargs):
+        new_color = False if not 'new_color' in kwargs else bool(kwargs['new_color'])
+
         img = img.convert('RGBA')
         toned = img.convert('L')
-        toned = ImageOps.colorize(toned, (27, 31, 51), (255, 255, 255), (114, 137, 218))
+
+        if new_color:
+            toned = ImageOps.colorize(toned, (25, 30, 73), (255, 255, 255), (88, 101, 241))
+        else:
+            toned = ImageOps.colorize(toned, (27, 31, 51), (255, 255, 255), (114, 137, 218))
 
         base = Image.new('RGBA', img.size)
         base.paste(toned, (0, 0), img)
